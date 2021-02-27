ed64223bc3ab4cd5efc9d5a865c663a84177b024
Justin Howe <howejust@gmail.com>
2021-02-27 11:24:25 +0800

    First Test Commit

--- src/test/java/st/Task1_Functional.java
+++ src/test/java/st/Task1_Functional.java
@@ -2,9 +2,22 @@ package st;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class Task1_Functional {
+	
+	private Parser parser;
+	
+	@Before
+	public void setup() {
+		parser = new Parser();
+	}
+	
+	@Test
+	public void test() {
+		assertEquals(5, 2+3);
+	}
 	
 
 }
