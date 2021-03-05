16b0cd2ce76c50abde007669c8e7660972693c55
Justin Howe <howejust@gmail.com>
2021-03-05 23:38:31 +0800

    Task Uno Completed; Duo to be finalized and Trio should be started soon

--- src/test/java/st/Task1_Functional.java
+++ src/test/java/st/Task1_Functional.java
@@ -18,21 +18,20 @@ public class Task1_Functional {
 	}
 			
 	// Find Bug 1 - 1.4 2 (Empty option name and shortcut)
-	@Test (expected = RuntimeException.class)
+	@Test
 	public void testBug1() {
-		parser.add("", "", 1);
+		parser.add("", "", Parser.STRING);
 	}
 	
 	// Find Bug 2 - 1.3 2 (Invalid shortcut name)
-	@Test (expected = RuntimeException.class)
+	@Test
 	public void testBug2() {		
-		// Test Case #1 - Invalid name
-		String optionName = "#*(*#@";
-		parser.add(optionName, "o", Parser.STRING);
+		String shortCutName = "#*(*#@";
+		parser.add(shortCutName, "o", Parser.STRING);
 	}
 	
-	// Find Bug 3 - 1.4 1
-	@Test(expected = RuntimeException.class)
+	// Find Bug 3 - 1.4 1 (Overriding)
+	@Test
 	public void testBug3() {
 		// Simple test case
 		parser.add("optimize", Parser.INTEGER);
@@ -43,7 +42,6 @@ public class Task1_Functional {
 	// Find Bug 4 - 1.3.5
 	@Test
 	public void testBug4() {
-		// Simple test case which fails
 		parser.add("optimise", "O", Parser.BOOLEAN);
 		int test = parser.parse("-O=0");
 		boolean result = parser.getBoolean("optimise");
@@ -57,7 +55,7 @@ public class Task1_Functional {
 		assertEquals(result,'\0');
 	}
 	
-	// Find Bug 6 - 1.5.4 (Problem with parsing the value as single quotations)
+	// Find Bug 6 - Problem with parsing the value as single quotations
 	@Test
 	public void testBug6() {
 		parser.add("option", "o", Parser.STRING);
@@ -66,45 +64,54 @@ public class Task1_Functional {
 		assertEquals(value,value2);
 	}
 	
+	// Bug 7 - Parsing negative value
+	@Test
+	public void testBug7() {
+		parser.add("option", Parser.INTEGER);
+		parser.parse("--option=-2");
+		parser.getInteger("option");
+	}
+	
 	// Bug 8 - Add option name as shortcut and parse with invalid shortcut
-	@Test (expected = RuntimeException.class)
+	@Test
 	public void testBug8() {
-		parser.add("k", "", 2);
+		parser.add("k", "", Parser.STRING);
 		parser.parse("-=sZ_wrMcah(");
 	}
 	
-	// Find Bug 9 - Needs Refactoring
-	@Test (expected = RuntimeException.class)
+	// Find Bug 9 - Parses with more than 50 characters
+	@Test
 	public void testBug9() {
-	    parser.parse("OptionMap [options=\n\t{name=test, shortcut=, type=4, value=}\n]");
+		parser.add("test", "", Parser.STRING);
+		parser.parse("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXY");
 	}
 	
 	// Bug 10 - Parse with newline character
-	@Test (expected = RuntimeException.class)
+	@Test
 	public void testBug10() {
-		parser.add("v", "", 2);
+		parser.add("v", "", Parser.STRING);
 		parser.parse("--v=\r");
 	}
 	
 	// Bug 11 - Parsing shortcut with an option name with length of 1
-	@Test (expected = RuntimeException.class)
+	@Test
 	public void testBug11() {
-		parser.add("v", "J", 2);
+		parser.add("v", "J", Parser.STRING);
 		parser.parse("-J=v");
 	}
 	
 	// Find Bug 12 - 1.5.1? (Command Line Options is empty)
-	@Test (expected = RuntimeException.class)
+	@Test
 	public void testBug12() {
 		int parseResult = parser.parse("");
 		assertEquals(parseResult,1);
 	}
 		
 	// Bug 13 - 1.3 4 (Option can have shortcut that has same name of another option)
-	@Test (expected = RuntimeException.class)
+	@Test
 	public void testBug13() {
-		parser.add("key", "value", 1);
-		parser.add("value", "key", 2);
+		parser.add("key", "value", Parser.STRING);
+		parser.add("value", "key", Parser.STRING);
 		parser.getInteger("value");
 	}
 
